fe691039726d19fce2658584e7c56c658f0c2540
Peter Giles <peter.s.giles@gmail.com>
2019-02-18 15:55:10 +1100

    fixed tests

--- projects/df-components/src/lib/deck/deck/deck.component.spec.ts
+++ projects/df-components/src/lib/deck/deck/deck.component.spec.ts
@@ -1,12 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing"
 
 import { DeckComponent } from "./deck.component"
-import { CardStandardComponent } from "../card-standard/card-standard.component"
-import { CardAudioComponent } from "../card-audio/card-audio.component"
-import { CardChartComponent } from "../card-chart/card-chart.component"
-import { CardImageComponent } from "../card-image/card-image.component"
-import { CardMarkdownComponent } from "../card-markdown/card-markdown.component"
-import { CardParentComponent } from "../card-parent/card-parent.component"
+
 
 describe("DeckComponent", () => {
   let component: DeckComponent
@@ -15,13 +10,7 @@ describe("DeckComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        DeckComponent,
-        CardAudioComponent,
-        CardChartComponent,
-        CardImageComponent,
-        CardMarkdownComponent,
-        CardParentComponent,
-        CardStandardComponent
+        DeckComponent
       ]
     }).compileComponents()
   }))
