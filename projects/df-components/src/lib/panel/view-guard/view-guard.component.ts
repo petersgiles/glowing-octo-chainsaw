03cae579aa0bc9c2233aafa970e83fe3b728b87d
Kim McClymont <Kim.McClymont@pmc.gov.au>
2019-05-21 13:47:08 +1000

    Added view guard for permissions

---
+++ projects/df-components/src/lib/panel/view-guard/view-guard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core'
+import {
+  OPERATION_RIGHT_WRITE,
+  OPERATION_RIGHT_READ,
+  OPERATION_RIGHT_HIDE
+} from './view-guard-operations'
+@Component({
+  selector: 'df-view-guard',
+  templateUrl: './view-guard.component.html',
+  styles: [``]
+})
+export class CommitmentViewGuardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  @Input()
+  operation
+
+  WRITE = OPERATION_RIGHT_WRITE
+  READ = OPERATION_RIGHT_READ
+  HIDE = OPERATION_RIGHT_HIDE
+}
