19d6896146542a55d04f8953e1b1bdbfd6b9b0c6
Kim McClymont <Kim.McClymont@pmc.gov.au>
2019-05-22 11:16:02 +1000

    test

--- projects/df-components-stories/src/lib/panel/panel.stories.ts
+++ projects/df-components-stories/src/lib/panel/panel.stories.ts
@@ -189,7 +189,18 @@ storiesOf("Panel", module)
   .add("View Guard", () => ({
     template: `<df-view-guard
     [operation]="operation">
-    
+    <div [ngSwitch]="operation">
+    <div *ngSwitchCase="READ">
+        <ng-content select="[operation-type=read]"></ng-content>
+    </div>
+    <div *ngSwitchCase="WRITE">
+        <ng-content select="[operation-type=write]"></ng-content>
+    </div>
+    <div *ngSwitchCase="HIDE">
+        <ng-content select="[operation-type=hide]"></ng-content>
+    </div>
+    <div *ngSwitchDefault></div>
+</div>
     <ng-container operation-type="read"><p>read only </p> </ng-container>
     </df-view-guard>
     `,
