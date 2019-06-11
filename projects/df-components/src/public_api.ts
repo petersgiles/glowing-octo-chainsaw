0d64570c3ba99d6f2d9c20b2e718bfbc8afbe39a
Peter Giles <peter.s.giles@gmail.com>
2019-06-11 15:30:35 +1000

    tweakin the api

--- projects/df-components/src/public_api.ts
+++ projects/df-components/src/public_api.ts
@@ -2,18 +2,17 @@
  * Public API Surface of df-components
  * Don't use barrelling here - make sure to reference index files explicitly: https://github.com/ng-packagr/ng-packagr/issues/355
  */
-
-export * from "./lib/filtered-list/index"
-export * from "./lib/refiner/index"
-export * from "./lib/data-table/index"
-export * from "./lib/discussion/index"
 export * from "./lib/avatar/index"
-export * from "./lib/panel/index"
 export * from "./lib/button/index"
-export * from "./lib/document/index"
+export * from "./lib/data-table/index"
 export * from "./lib/dialogs/index"
-export * from "./lib/mega-tagger/index"
+export * from "./lib/discussion/index"
+export * from "./lib/document/index"
 export * from "./lib/file/index"
+export * from "./lib/filtered-list/index"
 export * from './lib/layouts/index'
+export * from "./lib/mega-tagger/index"
+export * from "./lib/panel/index"
 export * from './lib/pipes/index'
+export * from "./lib/refiner/index"
 export * from './lib/utils/index'
